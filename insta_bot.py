71a00b7a99c464059c960da90cc4893736a46180
RajatBhardwaj123 <rajat.18bcan093@jecrcu.edu.in>
2019-10-07 18:59:00 +0530

    look1

---
+++ insta_bot.py
@@ -0,0 +1,6 @@
+from instabot import Bot
+
+bot=Bot()
+
+bot.login(username="rohitmehra7016",password="QwErTy321")
+bot.upload_photo("rohit2.jpg",caption="#randompic")
---
+++ insta_follower.py
@@ -0,0 +1,4 @@
+from instabot import Bot
+bot=Bot()
+bot.login(username="rohitmehra7016",password="QwErTy321")
+bot.follow_followers("mrrajatbhardwaj",10)
